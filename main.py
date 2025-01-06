2189bfce1768ce731957eb488f19058e94a4cfcf
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-06 20:10:29 +0000

    changes made

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+response = requests.get(url="https://api.npoint.io/28ade7d0524d0c348ff6")
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
