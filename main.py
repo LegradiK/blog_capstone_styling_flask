d8efdd9b9b415799ee45f561640da9942cc00189
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-07 12:01:05 +0000

    connecting json file with post.html

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+response = requests.get("https://api.npoint.io/674f5423f73deab1e9a7")
+response.raise_for_status()
+blog_articles = response.json()
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", blog=blog_articles)
+
+@app.route('/posts/<int:id>')
+def posts(id):
+    return render_template("post.html", blog=blog_articles, id=id)
 
 @app.route('/about')
 def about():
