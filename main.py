044b518e55abca72c2461ecc7db4db5428f7d1ea
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-09 10:52:50 +0000

    Added smtplib - still needs fixing

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request
 import requests
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 
 response = requests.get("https://api.npoint.io/674f5423f73deab1e9a7")
 response.raise_for_status()
 blog_articles = response.json()
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+receiver_email = os.getenv("RECIPIENT")
+gmail_address = os.getenv("GMAIL_ADDRESS")
+gmail_password = os.getenv("GMAIL_PASSWORD")
 
 @app.route('/')
 def home():
@@ -29,6 +39,27 @@ def contact():
         email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
+        # Create the email
+        subject = 'New Entry'
+        body = f'Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}'
+
+        # Set up the MIME structure
+        email_message = MIMEMultipart()
+        email_message['From'] = gmail_address
+        email_message['To'] = receiver_email
+        email_message['Subject'] = subject
+        email_message.attach(MIMEText(body, 'plain'))
+        try:
+            # Establish a connection to the SMTP server
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()  # Upgrade the connection to secure encrypted mode
+            server.login(gmail_address, gmail_password)  # Use app password
+            server.sendmail(gmail_address, receiver_email, email_message.as_string())
+            print("Email has been successfully sent.")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+        finally:
+            server.quit()
         return render_template("contact.html", method=method)
 
 
